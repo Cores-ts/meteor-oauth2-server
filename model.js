b3507ca118ea36b5ab4cb9df8c6beb64f5082274
Марко Кажић <marko.kazic@zamphyr.com>
2018-09-06 15:58:39 +0200

    refactor(Model): Starting model refactoring to avoid Meteor Fibers requirement

--- model.js
+++ model.js
@@ -1,112 +1,12 @@
 MeteorModel = {
-    getAccessToken: Meteor.bindEnvironment(
-        async function (bearerToken) {
-            try {
-                let token = await oAuth2Server.collections.accessToken.findOne({
-                    accessToken: bearerToken
-                })
-                return token
-            } catch (error) {
-
-            }
-        }),
-    getAuthorizationCode: Meteor.bindEnvironment(
-        async function (code) {
-            try {
-                let authCode = oAuth2Server.collections.authCode.findOne({
-                    authCode: code
-                })
-                return authCode
-            } catch (error) {
-                // STUB
-            }
+    getClient: async function (clientId, clientSecret) {
+        let client = await clientsCollection.rawCollection().findOne({
+            active: true,
+            clientId: clientId
+        })
+        return {
+            id: client.clientId,
+            grants: client.grantsAllowed
         }
-    ),
-    getRefreshToken: Meteor.bindEnvironment(
-        async function (bearerToken) {
-            try {
-                let token = await oAuth2Server.collections.refreshToken.findOne({
-                    refreshToken: bearerToken
-                })
-                return token
-            } catch (error) {
-                // STUB
-            }
-        }
-    ),
-    getClient: Meteor.bindEnvironment(
-        async function (clientId, clientSecret) {
-            try {
-                let client;
-                if (clientSecret == null) {
-                    client = await oAuth2Server.collections.client.findOne({
-                        active: true,
-                        clientId: clientId
-                    })
-                } else {
-                    client = await oAuth2Server.collections.client.findOne({
-                        active: true,
-                        clientId: clientId,
-                        clientSecret: clientSecret
-                    })
-                }
-                return client
-            } catch (e) {}
-        }),
-    getUser: Meteor.bindEnvironment(
-        async function (email, password) {
-            try {
-                let user = await Meteor.users.findOne({
-                    'profile.email': email,
-                    'services.password.bcrypt': Accounts.hashPassword(password)
-                })
-                return user
-            } catch (error) {
-
-            }
-        }
-    ),
-    saveAuthorizationCode: Meteor.bindEnvironment(
-        async function (code, clientId, expires, user) {
-            try {
-                oAuth2Server.collections.authCode.remove({
-                    authCode: code
-                });
-
-                oAuth2Server.collections.authCode.remove({
-                    clientId: clientId,
-                    userId: user.id
-                })
-
-                let codeId = await oAuth2Server.collections.authCode.insert({
-                    authCode: code,
-                    clientId: clientId,
-                    userId: user.id,
-                    expires: expires
-                });
-
-                return codeId
-
-            } catch (err) {
-                throw new Error(err)
-            }
-        }),
-    saveToken: function () {}
+    }
 }
-
-
-// getUserFromClient(client, [callback])
-// saveToken(token, client, user, [callback])
-// saveAuthorizationCode(code, client, user, [callback])
-// revokeToken(token, [callback])
-// revokeAuthorizationCode(code, [callback])
-// validateScope(user, client, scope, [callback])
-// verifyScope(accessToken, scope, [callback])
-
-/**
- * OPTIONAL
- */
-
-// generateAccessToken(client, user, scope, [callback])
-// generateRefreshToken(client, user, scope, [callback])
-// generateAuthorizationCode(client, user, scope, [callback])
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /**
  * Load dependencies
  */
+const bodyParser = Npm.require('body-parser')
 const OAuth2Server = Npm.require('oauth2-server')
 const Request = OAuth2Server.Request;
 const Response = OAuth2Server.Response;
@@ -8,8 +9,8 @@ const Response = OAuth2Server.Response;
 /**
  * Configure server-side collections
  */
-const accessTokenCollection = new Meteor.Collection('OAuth2AccessTokens');
-const clientsCollection = new Meteor.Collection('OAuth2Clients');
+var accessTokenCollection = new Meteor.Collection('OAuth2AccessTokens');
+clientsCollection = new Meteor.Collection('OAuth2Clients');
 
 oAuth2Server.collections.accessToken = accessTokenCollection;
 oAuth2Server.collections.client = clientsCollection;
@@ -19,38 +20,44 @@ oAuth2Server.collections.client = clientsCollection;
  */
 
 oAuth2Server.oauthserver = new OAuth2Server({
-    model: MeteorModel,
-    allowEmptyState: true
+    model: MeteorModel
 });
-
 
 /**
  * Handle routes
+ * This code tests use of WebApp for middleware will be removed and shouldn't be a template for futher development
  */
 
-// This code tests use of WebApp for middleware will be removed and shouldn't be a template for futher development
-WebApp.connectHandlers.use('/oauth/token', (req, res, next) => {
-    res.writeHead(200);
-    let request = new Request(req);
-    let response = new Response(res);
-    return oAuth2Server.oauthserver.authenticate(request, response)
-        .then(function (token) {
-            res.end(token)
-            next()
-        }).catch(function (err) {});
-});
+/**
+ * Reimplement authorize here instead of the method
+ */
 
-WebApp.connectHandlers.use('/oauth/authorization_code', (req, res, next) => {
-    res.writeHead(200);
-    let request = new Request(req);
-    let response = new Response(res);
-    return oAuth2Server.oauthserver.generateAuthorizationCode({}, {}, null)
-        .then(function (code) {
-            console.log(code)
-            next()
-        }).catch(function (err) {});
-});
+// WebApp.connectHandlers.use('/oauth/authorize', (req, res, next) => {
+//     res.writeHead(200);
+//     let request = new Request(req);
+//     let response = new Response(res);
+//     return oAuth2Server.oauthserver.authenticate(request, response)
+//         .then(function (token) {
+//             res.end(token)
+//             next()
+//         }).catch(function (err) {
+//             console.error(err)
+//             res.end()
+//         });
+// });
 
+
+WebApp.connectHandlers
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use('/oauth/token', (req, res, next) => {
+        let request = new Request(req);
+        let response = new Response(res);
+        console.log(request)
+        oAuth2Server.oauthserver.token(request, response, {
+            requireClientAuthentication: { password: false }
+        })
+        next()
+    })
 /**
  * Publish data
  */
@@ -80,25 +87,3 @@ Meteor.publish(oAuth2Server.pubSubNames.refreshTokens, function () {
         }
     });
 });
-
-/**
- * Meteor methods
- */
-
-Meteor.methods({
-    'authorize'(clientId, redirectUri, responseType, scope, state) {
-        check(clientId, String);
-        check(redirectUri, String);
-        check(responseType, String);
-        check(scope, Match.Optional(Match.OneOf(null, [String])));
-        check(state, Match.Optional(Match.OneOf(null, String)));
-
-        let userId = this.userId;
-        if (!userId) {
-            return {
-                success: false,
-                error: 'User not authenticated.'
-            }
-        }
-    }
-})
